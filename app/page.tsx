84baf0fca82bebdc38b48c3c09dfa4d9684de4d7
Anshul6708 <78428235+Anshul6708@users.noreply.github.com>
2025-06-08 12:57:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-[#1C1C1C] text-white p-4">
       <div className="flex flex-col items-center mt-[20vh]">
-        <h1 className="text-7xl font-light mb-8">Wavelength</h1>
+        <h1 className="text-5xl font-light mb-8">Wavelength</h1>
         <p className="text-3xl text-gray-400 italic mb-8">Only meet people of your wavelength</p>
         <button
           onClick={() => router.push('/chat')}
